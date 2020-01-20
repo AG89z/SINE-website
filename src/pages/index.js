aa2d14d9bb8a768985be330dfc098ccc2b8ea9b1
AG89Z <agiachino@zmorph3d.com>
2020-01-20 20:49:13 +0100

    Starting to work

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,36 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+import { makeStyles } from "@material-ui/styles"
+
+const useStyles = makeStyles({
+  bannerImage: {
+    width: "100vw",
+  },
+})
+
+export default () => {
+  const classes = useStyles()
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "images/sine banner 1.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1800) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div>
+      <Img
+        className={classes.bannerImage}
+        fluid={data.file.childImageSharp.fluid}
+        alt="Sine banner 1"
+      />
+    </div>
+  )
+}
